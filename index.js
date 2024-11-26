02ca698a2932fd32e4b7cf2e71a10f1039351202
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2024-11-26 12:37:20 -0500

    Add operator function with switch for operations

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ function divide(num1, num2) {
     }
     return num1 / num2;
 }
+
+let result; 
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
